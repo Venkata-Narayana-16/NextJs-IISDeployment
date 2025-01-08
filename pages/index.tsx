629f6f55e8a92c3d7122f3c0f9b9bde7d3a26f5a
Venkata-Narayana-16 <venkatnarayanakuruva@gmail.com>
2025-01-08 23:39:27 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hi Venkat-DevOpsEngineer, Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hi Mr.DevOpsEngineer, Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
